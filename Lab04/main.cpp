3bc78a7934ffdd4775c5b61cc10da6380901391b
Sinéad Galbraith <SineadGalbraith@users.noreply.github.com>
2021-01-03 23:16:23 +0000

    Finished game with text added to the screen.

--- Lab04/main.cpp
+++ Lab04/main.cpp
@@ -2,6 +2,7 @@
 #include <windows.h>
 #include <mmsystem.h>
 #include <iostream>
+#include <map>
 //#include <stdio.h>
 
 // Include GLUT
@@ -23,6 +24,10 @@
 // Include Model Class
 #include "Model.h"
 Model model;
+
+// Include FreeType
+#include <ft2build.h>
+#include FT_FREETYPE_H 
 
 /*----------------------------------------------------------------------------
 MESHES
@@ -97,7 +102,7 @@ bool keyO = false;
 bool keyP = false;
 
 // ------------ SHADERS ------------
-GLuint textureShaderProgramID, skyboxShaderProgramID, objectShaderProgramID;
+GLuint textureShaderProgramID, skyboxShaderProgramID, objectShaderProgramID, fontShaderProgramID;
 float uvScalar = 0;
 
 // ------------ VBO/VAO SETUP ------------
@@ -131,6 +136,8 @@ glm::vec3 lastCameraUp;
 
 // ------------ FRED (I.E PLAYER CHARACTER) ------------
 glm::vec3 fredPos = glm::vec3(0.0f, 17.0f, 222.0f);
+int stepCounter = 0;
+int stepTotal = 452;
 
 // ------------ SCORE ------------
 int playerScore = 0;
@@ -182,6 +189,7 @@ glm::vec3 bag4Pos = glm::vec3(-15.5f, 4.0f, -100.0f);
 glm::vec3 bag5Pos = glm::vec3(20.0f, 5.0f, -180.0f);
 
 // ------------ BINS ------------
+int binDistance = 20;
 glm::vec3 bin1Pos = glm::vec3(-20.0, 3.0, 135.0f);
 glm::vec3 bin2Pos = glm::vec3(20.0, 3.0, 20.0f);
 glm::vec3 bin3Pos = glm::vec3(-20.0, 3.0, -100.0f);
@@ -243,6 +251,128 @@ float skyboxVertices[] = {
 	-500.0f, -500.0f,  500.0f,
 	 500.0f, -500.0f,  500.0f
 };
+
+// ------------ FREETYPE/TEXT ------------
+#pragma region TEXT
+unsigned int fontVAO, fontVBO;
+
+// This struct is used to store the individual character information.
+struct Character {
+	unsigned int TextureID;  
+	glm::ivec2   Size;      
+	glm::ivec2   Bearing;   
+	unsigned int Advance;    
+};
+
+std::map<char, Character> Characters;
+
+int createFont() {
+	FT_Library ft;
+	if (FT_Init_FreeType(&ft))
+	{
+		std::cout << "ERROR::FREETYPE: Could not init FreeType Library" << std::endl;
+		return -1;
+	}
+	FT_Face face;
+	FT_New_Face(ft, "./fonts/arial.ttf", 0, &face);
+	if (FT_New_Face(ft, "./fonts/arial.ttf", 0, &face))
+	{
+		std::cout << "ERROR::FREETYPE: Failed to load font" << std::endl;
+		return -1;
+	}
+	else {
+		FT_Set_Pixel_Sizes(face, 0, 25);
+
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction
+
+		for (unsigned char c = 0; c < 128; c++)
+		{
+			// load character glyph 
+			if (FT_Load_Char(face, c, FT_LOAD_RENDER))
+			{
+				std::cout << "ERROR::FREETYTPE: Failed to load Glyph" << std::endl;
+				continue;
+			}
+			// generate texture
+			unsigned int texture;
+			glGenTextures(1, &texture);
+			glBindTexture(GL_TEXTURE_2D, texture);
+			glTexImage2D(
+				GL_TEXTURE_2D,
+				0,
+				GL_RED,
+				face->glyph->bitmap.width,
+				face->glyph->bitmap.rows,
+				0,
+				GL_RED,
+				GL_UNSIGNED_BYTE,
+				face->glyph->bitmap.buffer
+			);
+			// set texture options
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+			// now store character for later use
+			Character character = {
+				texture,
+				glm::ivec2(face->glyph->bitmap.width, face->glyph->bitmap.rows),
+				glm::ivec2(face->glyph->bitmap_left, face->glyph->bitmap_top),
+				face->glyph->advance.x
+			};
+			Characters.insert(std::pair<char, Character>(c, character));
+		}
+		glBindTexture(GL_TEXTURE_2D, 0);
+	}
+	
+	FT_Done_Face(face);
+	FT_Done_FreeType(ft);
+}
+
+void RenderText(GLuint shaderProgramID, std::string text, float x, float y, float scale, glm::vec3 color)
+{
+	// activate corresponding render state	
+	glUseProgram(shaderProgramID);
+	glUniform3f(glGetUniformLocation(shaderProgramID, "textColor"), color.x, color.y, color.z);
+	glActiveTexture(GL_TEXTURE0);
+	glBindVertexArray(fontVAO);
+
+	// iterate through all characters
+	std::string::const_iterator c;
+	for (c = text.begin(); c != text.end(); c++)
+	{
+		Character ch = Characters[*c];
+
+		float xpos = x + ch.Bearing.x * scale;
+		float ypos = y - (ch.Size.y - ch.Bearing.y) * scale;
+
+		float w = ch.Size.x * scale;
+		float h = ch.Size.y * scale;
+		// update VBO for each character
+		float vertices[6][4] = {
+			{ xpos,     ypos + h,   0.0f, 0.0f },
+			{ xpos,     ypos,       0.0f, 1.0f },
+			{ xpos + w, ypos,       1.0f, 1.0f },
+
+			{ xpos,     ypos + h,   0.0f, 0.0f },
+			{ xpos + w, ypos,       1.0f, 1.0f },
+			{ xpos + w, ypos + h,   1.0f, 0.0f }
+		};
+		// render glyph texture over quad
+		glBindTexture(GL_TEXTURE_2D, ch.TextureID);
+		// update content of VBO memory
+		glBindBuffer(GL_ARRAY_BUFFER, fontVBO);
+		glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(vertices), vertices);
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+		// render quad
+		glDrawArrays(GL_TRIANGLES, 0, 6);
+		// now advance cursors for next glyph (note that advance is number of 1/64 pixels)
+		x += (ch.Advance >> 6) * scale; // bitshift by 6 to get value in pixels (2^6 = 64)
+	}
+	glBindVertexArray(0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+}
+#pragma endregion TEXT
 
 // ------------ SHADER FUNCTIONS (UNCHANGED) ------------
 #pragma region SHADER_FUNCTIONS
@@ -493,6 +623,18 @@ void generateNonTextureObjects(std::vector <ModelData> nonTextureObjects) {
 
 		counter += 2;
 	}
+}
+
+void generateFontBufferObjects() {
+	glGenVertexArrays(1, &fontVAO);
+	glGenBuffers(1, &fontVBO);
+	glBindVertexArray(fontVAO);
+	glBindBuffer(GL_ARRAY_BUFFER, fontVBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(float) * 6 * 4, NULL, GL_DYNAMIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), 0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
 }
 #pragma endregion VBO_FUNCTIONS
 
@@ -758,10 +900,10 @@ void display() {
 	glBindVertexArray(VAO[0]);
 
 	// Deposit Objects Into Bin
-	if (glm::distance(bin1Pos.x, fredPos.x) < xDistance & glm::distance(bin1Pos.z, fredPos.z) < zDistance || 
-		glm::distance(bin2Pos.x, fredPos.x) < xDistance & glm::distance(bin2Pos.z, fredPos.z) < zDistance || 
-		glm::distance(bin3Pos.x, fredPos.x) < xDistance & glm::distance(bin3Pos.z, fredPos.z) < zDistance || 
-		glm::distance(bin4Pos.x, fredPos.x) < xDistance & glm::distance(bin4Pos.z, fredPos.z) < zDistance) {
+	if (glm::distance(bin1Pos.x, fredPos.x) < xDistance & glm::distance(bin1Pos.z, fredPos.z) < binDistance || 
+		glm::distance(bin2Pos.x, fredPos.x) < xDistance & glm::distance(bin2Pos.z, fredPos.z) < binDistance || 
+		glm::distance(bin3Pos.x, fredPos.x) < xDistance & glm::distance(bin3Pos.z, fredPos.z) < binDistance || 
+		glm::distance(bin4Pos.x, fredPos.x) < xDistance & glm::distance(bin4Pos.z, fredPos.z) < binDistance) {
 		pressM = true;
 	}
 	else {
@@ -1339,14 +1481,79 @@ void display() {
 		}
 	}
 
-	// ------------------------------------- End Game ------------------------------------- 
-	if (fredPos.z <= -210) {
-		std::string score = "Final Score:\n " + std::to_string(playerScore);
+	// ------------------------------------- FONT ------------------------------------- (font Shader)
+	glEnable(GL_BLEND);
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+	glm::mat4 projection = glm::ortho(0.0f, 800.0f, 0.0f, 600.0f);
+	glUseProgram(fontShaderProgramID);
+	glUniformMatrix4fv(glGetUniformLocation(fontShaderProgramID, "proj"), 1, GL_FALSE, glm::value_ptr(projection));
+
+	// Display Score
+	RenderText(fontShaderProgramID, "Score: " + std::to_string(playerScore), 660.0f, 550.0f, 1.0f, glm::vec3(0.3f, 0.3f, 0.3f));
+
+	// Show Camera Movement Buttons
+	RenderText(fontShaderProgramID, "Camera", 50.0f, 570.0f, 1.0f, glm::vec3(1.0f));
+	RenderText(fontShaderProgramID, "W", 40.0f, 540.0f, 1.0f, glm::vec3(1.0f));
+	RenderText(fontShaderProgramID, "A", 10.0f, 510.0f, 1.0f, glm::vec3(1.0f));
+	RenderText(fontShaderProgramID, "S", 42.0f, 480.0f, 1.0f, glm::vec3(1.0f));
+	RenderText(fontShaderProgramID, "D", 75.0f, 510.0f, 1.0f, glm::vec3(1.0f));
+	RenderText(fontShaderProgramID, "U", 120.0f, 535.0f, 1.0f, glm::vec3(1.0f));
+	RenderText(fontShaderProgramID, "J", 120.0f, 485.0f, 1.0f, glm::vec3(1.0f));
+	RenderText(fontShaderProgramID, "C - Look Around", 10.0f, 425.0f, 1.0f, glm::vec3(1.0f));
+	RenderText(fontShaderProgramID, "O - Overhead View", 10.0f, 395.0f, 1.0f, glm::vec3(1.0f));
+
+	// Display 'Press M' when near bin
+	if (pressM) {
+		if (bottleInventory > 0 || bagInventory > 0) {
+			RenderText(fontShaderProgramID, "Press M", 350.0f, 450.0f, 1.0f, glm::vec3(0.0f));
+		}
+	}
+	
+	// Display 'Press P' when near object
+	if (pressP) {
+		RenderText(fontShaderProgramID, "Press P", 350.0f, 500.0f, 1.0f, glm::vec3(0.0f));
+	}
+
+	// Display R if camera view is different than FredPos 
+	if (cameraPosition.x != fredPos.x || cameraPosition.y != fredPos.y || (glm::distance(cameraPosition.z, fredPos.z) > 5)) {
+		RenderText(fontShaderProgramID, "Press R to Reset View", 275.0f, 400.0f, 1.0f, glm::vec3(0.0f));
+	}
+
+	// Show Inventory
+	RenderText(fontShaderProgramID, "Inventory", 660.0f, 480.0f, 1.0f, glm::vec3(0.3f, 0.3f, 0.3f));
+	RenderText(fontShaderProgramID, "Bottles: " + std::to_string(bottleInventory), 660.0f, 450.0f, 1.0f, glm::vec3(0.3f, 0.3f, 0.3f));
+	RenderText(fontShaderProgramID, "Bags: " + std::to_string(bagInventory), 660.0f, 420.0f, 1.0f, glm::vec3(0.3f, 0.3f, 0.3f));
+
+	// Show Quit Option
+	RenderText(fontShaderProgramID, "Press X to Quit", 620.0f, 10.0f, 1.0f, glm::vec3(1.0f));
+	
+	// ------------------------------------- PROGRESS ------------------------------------- 
+	int progress = (float(stepCounter) / float(stepTotal)) * 100 ;
+	RenderText(fontShaderProgramID, "Game Progress: " + std::to_string(progress) + "%" , 10.0f, 10.0f, 1.0f, glm::vec3(1.0f));
+
+	
+	// ------------------------------------- END GAME ------------------------------------- 
+
+	if (playerScore == 25) {
+		std::string score = "You Won! Congratulations!\nYou collected all of the waste items :)\nYour final score is " + std::to_string(playerScore) + "\nThank you for playing :)";
 		LPCSTR sw = score.c_str();
-		int messageBoxID = MessageBox(NULL, sw, "Game Over", MB_OK);
+		int messageBoxID = MessageBox(NULL, sw, "Game Over!", MB_OK);
 		switch (messageBoxID) {
 		case IDOK:
 			exit(0);
+		}
+	}
+
+	if (fredPos.z <= -200) {
+		if (bottleInventory != 10 && bagInventory != 5) {
+			std::string score = "You Lost! You failed to collect all of the waste items :(\nYour final score is " + std::to_string(playerScore) + "\nThank you for playing!";
+			LPCSTR sw = score.c_str();
+			int messageBoxID = MessageBox(NULL, sw, "Game Over!", MB_OK);
+			switch (messageBoxID) {
+			case IDOK:
+				exit(0);
+			}
 		}
 	}
 
@@ -1379,11 +1586,14 @@ void init()
 	textureShaderProgramID = CompileShaders("./shaders/textureVertexShader.txt", "./shaders/textureFragmentShader.txt");
 	skyboxShaderProgramID = CompileShaders("./shaders/skyboxVertexShader.txt", "./shaders/skyboxFragmentShader.txt");
 	objectShaderProgramID = CompileShaders("./shaders/objectVertexShader.txt", "./shaders/objectFragmentShader.txt");
+	fontShaderProgramID = CompileShaders("./shaders/fontVertexShader.txt", "./shaders/fontFragmentShader.txt");
 	// generate Skybox and load Skybox images
 	generateSkybox();
 	cubemapTexture = loadCubemap(faces);
 	// Generate the rest of the Models (both with textures and without)
 	generateModels();
+	createFont();
+	generateFontBufferObjects();
 }
 
 // ------------ KEYBOARD AND MOUSE FUNCTIONALITY ------------
@@ -1405,14 +1615,6 @@ void keypress(unsigned char key, int x, int y) {
 			fredPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 		}
 		break;
-	// Show Score
-	case 'b':
-		messageBoxID = MessageBox(NULL, scores, "Score", MB_OK);
-		switch (messageBoxID) {
-		case IDOK:
-			break;
-		}
-		break;
 	// Use camera to move around the screen
 	case 'c':
 		if (!keyC && !keyO) {
@@ -1429,14 +1631,6 @@ void keypress(unsigned char key, int x, int y) {
 			fredPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
 		}
 		break;
-	// Inventory
-	case 'i':
-		messageBoxID = MessageBox(NULL, sw, "Inventory", MB_OK);
-		switch (messageBoxID) {
-		case IDOK:
-			break;
-		}
-		break;
 	// Move Camera Down
 	case 'j':
 		if (cameraPosition.y >= 10 && !keyO) {
@@ -1445,7 +1639,7 @@ void keypress(unsigned char key, int x, int y) {
 		break;
 	// Deposit Inventory Items into Bin
 	case 'm':
-		if (pressM && (bottleInventory != 0) & (bagInventory != 0)) {
+		if (pressM) {
 			keyM = true;
 			playerScore += bottleInventory;
 			playerScore += bagInventory * 3;
@@ -1482,6 +1676,7 @@ void keypress(unsigned char key, int x, int y) {
 	// Move Camera Backwards
 	case 's':
 		if (cameraPosition.z < 220 && !keyO) {
+			stepCounter--;
 			cameraPosition -= cameraSpeed * cameraFront;
 			fredPos -= cameraSpeed * cameraFront;
 		}
@@ -1495,6 +1690,7 @@ void keypress(unsigned char key, int x, int y) {
 	// Move Camera Forwards
 	case 'w':
 		if (cameraPosition.z > -300 && !keyO) {
+			stepCounter++;
 			cameraPosition += cameraSpeed * cameraFront;
 			fredPos += cameraSpeed * cameraFront;
 		}
